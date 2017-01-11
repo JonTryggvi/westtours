fbc6c9059cee48bea2f0aa795ee33b53a7e6e3ce
Jón Tryggvi Unnarsson <jontryggvi@jontryggvi.is>
2017-01-11 09:08:14 +0000

    dropdown nav

--- assets/javascript/custom/script.js
+++ assets/javascript/custom/script.js
@@ -1,1 +1,13 @@
 
+var onScroll = function(container, theclass, amount) {
+		var wrap = $(container);
+		$(window).scroll(function(){
+			if ($(document).scrollTop() > amount) {
+				wrap.addClass(theclass);
+			} else {
+			  wrap.removeClass(theclass);
+			}
+		});
+	};
+
+    onScroll('.dropdown-main-container','set-nav-position', 24 );
--- template-parts/nav-frontpage.php
+++ template-parts/nav-frontpage.php
@@ -1,22 +1,22 @@
-<nav>
+
   <div id="site-navigation" class="main-navigation top-bar row" role="navigation">
     <div class="top-bar-left large-3">
       <ul class="menu">
         <li class="home"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><img src="<?php echo get_template_directory_uri() . '/assets/images/icons/logo.svg' ?>" alt=""/></a></li>
       </ul>
     </div>
-    <div class="top-bar-left large-4 large-offset-1">
+    <div class="top-bar-left centerMenu large-4">
       <?php foundationpress_top_bar_l(); ?>
 
       <?php if ( ! get_theme_mod( 'wpt_mobile_menu_layout' ) || get_theme_mod( 'wpt_mobile_menu_layout' ) === 'topbar' ) : ?>
         <?php get_template_part( 'template-parts/mobile-top-bar' ); ?>
       <?php endif; ?>
     </div>
-    <div class="top-bar-left large-4">
+    <div class="top-bar-left large-4 large-offset-1">
       <ul class="nav-filter align-right">
         <li class="lang">
           <select class="easy-dropdown" name="">
-            <option value="icelandic">Icelandic</option>
+            <option value="icelandic">Íslenska</option>
             <option value="english">English</option>
           </select>
         </li>
@@ -24,4 +24,3 @@
       </ul>
     </div>
   </div>
-</nav>
